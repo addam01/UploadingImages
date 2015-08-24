2ab2724ca34426f15e6e7ba928fc91cffd97d053
addam01 <addam01@hotmail.com>
2015-08-24 16:56:01 +0800

    Done to step 10 in http://www.androidhive.info/2014/12/android-uploading-camera-image-video-to-server-with-progress-bar/

---
+++ app/src/main/java/testaccountlogin/rounin_rave/com/uploadingimages/Config.java
@@ -0,0 +1,13 @@
+package testaccountlogin.rounin_rave.com.uploadingimages;
+
+/**
+ * Created by IAT Dev on 24/08/2015.
+ */
+public class Config {
+    //The online path for php
+    public static final String FILE_UPLOAD_URL = "http://www.krowdcap.com/fileUpload.php";
+
+    // Directory name to store captured images and videos
+    public static final String IMAGE_DIRECTORY_NAME = "Android File Upload";
+}
+
