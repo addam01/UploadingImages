339f9ab2f0f63846f7ab2a8aa5fe271d7e8db60c
addam01 <addam01@hotmail.com>
2015-08-26 20:51:26 +0800

    Completed rewrite code from https://trinitytuts.com/capture-image-upload-server-android/
    Error includes from the URL of the php.. please check

--- app/src/main/java/testaccountlogin/rounin_rave/com/uploadingimages/MainActivity.java
+++ app/src/main/java/testaccountlogin/rounin_rave/com/uploadingimages/MainActivity.java
@@ -33,6 +33,7 @@ import org.apache.http.util.EntityUtils;
 import java.io.ByteArrayOutputStream;
 import java.util.ArrayList;
 
+@SuppressWarnings("ALL")
 public class MainActivity extends AppCompatActivity {
 
     private ImageView imageView;
@@ -53,9 +54,7 @@ public class MainActivity extends AppCompatActivity {
 
         imageView = (ImageView) findViewById(R.id.ImagePrev);
         buttonCapture = (Button) findViewById(R.id.SnapPic);
-
     }
-
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -98,6 +97,97 @@ public class MainActivity extends AppCompatActivity {
 
         // get the file url
         fileUri = savedInstanceState.getParcelable("file_uri");
+    }
+
+    public void uploadToServer(View view) {
+        // Image location URL
+        Log.e("path", "----------------" + picturePath);
+
+        // Image
+        Bitmap bm = BitmapFactory.decodeFile(picturePath);
+        ByteArrayOutputStream bao = new ByteArrayOutputStream();
+        bm.compress(Bitmap.CompressFormat.JPEG, 90, bao);
+        byte[] ba = bao.toByteArray();
+        ba1 = Base64.encodeToString(ba,Base64.DEFAULT);
+
+        Log.e("base64", "-----" + ba1);
+
+        // Upload image to server
+        new uploadToServer().execute();
+
+    }
+
+    public void clickPic(View view) {
+        // Check Camera
+        if (getApplicationContext().getPackageManager().hasSystemFeature(
+                PackageManager.FEATURE_CAMERA)) {
+            // Open default camera
+            Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+            intent.putExtra(MediaStore.EXTRA_OUTPUT, fileUri);
+
+            // start the image capture Intent
+            startActivityForResult(intent, 100);
+
+        } else {
+            Toast.makeText(getApplication(), "Camera not supported", Toast.LENGTH_LONG).show();
+        }
+    }
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == 100 && resultCode == RESULT_OK) {
+
+            selectedImage = data.getData();
+            photo = (Bitmap) data.getExtras().get("data");
+
+            // Cursor to get image uri to display
+
+            String[] filePathColumn = {MediaStore.Images.Media.DATA};
+            Cursor cursor = getContentResolver().query(selectedImage,
+                    filePathColumn, null, null, null);
+            cursor.moveToFirst();
+
+            int columnIndex = cursor.getColumnIndex(filePathColumn[0]);
+            picturePath = cursor.getString(columnIndex);
+            cursor.close();
+
+            Bitmap photo = (Bitmap) data.getExtras().get("data");
+            imageView.setImageBitmap(photo);
+        }
+    }
+    public class uploadToServer extends AsyncTask<Void, Void, String> {
+
+//        private ProgressDialog pd = new ProgressDialog(getApplication());
+//        protected void onPreExecute() {
+//            super.onPreExecute();
+//            pd.setMessage("Wait image uploading!");
+//            pd.show();
+//        }
+
+        @Override
+        protected String doInBackground(Void... params) {
+
+            ArrayList<NameValuePair> nameValuePairs = new ArrayList<NameValuePair>();
+            nameValuePairs.add(new BasicNameValuePair("base64", ba1));
+            nameValuePairs.add(new BasicNameValuePair("ImageName", System.currentTimeMillis() + ".jpg"));
+            try {
+                HttpClient httpclient = new DefaultHttpClient();
+                HttpPost httppost = new HttpPost(Server_config.SERVER_URL);
+                httppost.setEntity(new UrlEncodedFormEntity(nameValuePairs));
+                HttpResponse response = httpclient.execute(httppost);
+                String st = EntityUtils.toString(response.getEntity());
+                Log.v("log_tag", "In the try Loop" + st);
+
+            } catch (Exception e) {
+                Log.v("log_tag", "Error in http connection " + e.toString());
+            }
+            return "Success";
+
+        }
+
+        protected void onPostExecute(String result) {
+            super.onPostExecute(result);
+//            pd.hide();
+//            pd.dismiss();
+        }
     }
 
 }
