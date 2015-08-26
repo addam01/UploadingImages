3a9e6ecabda413eb2782fec41f688477facca213
addam01 <addam01@hotmail.com>
2015-08-26 20:25:00 +0800

    Done to step 10 in http://www.androidhive.info/2014/12/android-uploading-camera-image-video-to-server-with-progress-bar/

--- app/src/main/java/testaccountlogin/rounin_rave/com/uploadingimages/MainActivity.java
+++ app/src/main/java/testaccountlogin/rounin_rave/com/uploadingimages/MainActivity.java
@@ -1,14 +1,19 @@
 package testaccountlogin.rounin_rave.com.uploadingimages;
 
 import android.app.Fragment;
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.database.Cursor;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.net.Uri;
+import android.os.AsyncTask;
 import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Base64;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -16,18 +21,30 @@ import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.Toast;
 
+import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicNameValuePair;
+import org.apache.http.util.EntityUtils;
+
+import java.io.ByteArrayOutputStream;
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
-
-    // Activity Request Codes
-    private int CAMERA_CAPTURE_IMAGE_REQUEST_CODE = 100;
-    private int CAMERA_CAPTURE_VIDEO_REQUEST_CODE = 200;
-    private int MEDIA_TYPE_IMAGE = 1;
-    private int MEDIA_TYPE_VIDEO = 2;
-
-    private Uri fileUri; //file url to store image
 
     private ImageView imageView;
     private Button buttonCapture;
+    // Activity Request Codes
+    private int CAMERA_CAPTURE_IMAGE_REQUEST_CODE = 100;
+
+    private Uri fileUri; //file url to store image
+    String picturePath;
+    Uri selectedImage;
+    Bitmap photo;
+    String ba1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,79 +54,9 @@ public class MainActivity extends AppCompatActivity {
         imageView = (ImageView) findViewById(R.id.ImagePrev);
         buttonCapture = (Button) findViewById(R.id.SnapPic);
 
-        buttonCapture.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                captureImage();
-            }
-        });
     }
 
-    private void captureImage() {
-        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
 
-        fileUri = outputMedia.getOutputMediaFileUri(MEDIA_TYPE_IMAGE);
-
-        intent.putExtra(MediaStore.EXTRA_OUTPUT, fileUri);
-
-        // start the image capture Intent
-        startActivityForResult(intent, CAMERA_CAPTURE_IMAGE_REQUEST_CODE);
-    }
-    /**
-     * Checking device has camera hardware or not
-     * */
-    private boolean isDeviceSupportCamera() {
-        // this device has a camera no camera on this device
-        return getApplicationContext().getPackageManager().hasSystemFeature(
-                PackageManager.FEATURE_CAMERA);
-    }
-
-    /**
-            * Receiving activity result method will be called after closing the camera
-    * */
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        // if the result is capturing Image
-        if (requestCode == CAMERA_CAPTURE_IMAGE_REQUEST_CODE) {
-            if (resultCode == RESULT_OK) {
-                // successfully captured the image
-                // display it in image view
-                previewCapturedImage();
-            } else if (resultCode == RESULT_CANCELED) {
-                // user cancelled Image capture
-                Toast.makeText(getApplicationContext(),
-                        "User cancelled image capture", Toast.LENGTH_SHORT)
-                        .show();
-            } else {
-                // failed to capture image
-                Toast.makeText(getApplicationContext(),
-                        "Sorry! Failed to capture image", Toast.LENGTH_SHORT)
-                        .show();
-            }
-        }
-    }
-    /*
-     * Display image from a path to ImageView
-     */
-    private void previewCapturedImage() {
-        try {
-            imageView.setVisibility(View.VISIBLE);
-
-            // bimatp factory
-            BitmapFactory.Options options = new BitmapFactory.Options();
-
-            // downsizing image as it throws OutOfMemory Exception for larger
-            // images
-            options.inSampleSize = 8;
-
-            final Bitmap bitmap = BitmapFactory.decodeFile(fileUri.getPath(),
-                    options);
-
-            imageView.setImageBitmap(bitmap);
-        } catch (NullPointerException e) {
-            e.printStackTrace();
-        }
-    }
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
@@ -152,7 +99,5 @@ public class MainActivity extends AppCompatActivity {
         // get the file url
         fileUri = savedInstanceState.getParcelable("file_uri");
     }
-
-
 
 }
--- app/src/main/java/testaccountlogin/rounin_rave/com/uploadingimages/server_config.java
+++ app/src/main/java/testaccountlogin/rounin_rave/com/uploadingimages/server_config.java
@@ -1,8 +1,10 @@
 package testaccountlogin.rounin_rave.com.uploadingimages;
 
+/**
+ * Created by IAT Dev on 26/08/2015.
+ */
+public class Server_config {
 
-public class server_config {
-    public static final String FILE_UPLOAD_URL ="";
-    String IMAGE_DIRECTORY_NAME = "Testing File";
-
+    public static final String SERVER_URL = "http://www.krowdcap.com/App/TestUpload.php";
+    public static final String FILE_URL = "";
 }
